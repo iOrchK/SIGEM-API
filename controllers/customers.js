fb5f179bc02ff1dbcd4835c144cb98f7b638bb13
jchable <yahicimosclick9193@gmail.com>
2020-09-08 00:51:17 -0500

    Optimización de servicio de clientes

---
+++ controllers/customers.js
@@ -0,0 +1,151 @@
+var CreateResponse = require("../services/response");
+var Status = require("../config/utils/status.json");
+var Mysql = require("mysql");
+var uuid = require("uuid");
+require('../config/conection');
+
+module.exports.Search = async (req) => {
+    var connection = Mysql.createConnection(global.gConfig.database);
+    var database = global.gConfig.database.name;
+
+    let where = "";
+    if (req.query.id_cliente) {
+        where = `WHERE id_cliente = ${req.query.id_cliente}`;
+    }
+
+    let query = `SELECT * FROM ${database}.clientes ${where}`;
+    
+    return await new Promise((resolve) => {
+        connection.connect();
+        console.log("\nConexión establecida");
+
+        console.log(`Ejecutando consulta: ${query}`);
+        connection.query(query, (error, rows, fields) => {
+            connection.end();
+            console.log("Conexión cerrada");
+
+            if (error) {
+                resolve(
+                    CreateResponse(Status._500, error)
+                );
+            }
+
+            if (!rows || rows === null || rows === undefined || !rows.length) {
+                resolve(
+                    CreateResponse(Status._400, null)
+                );
+            }
+
+            resolve(
+                CreateResponse(Status._200, rows)
+            );
+        });
+    });
+}
+
+
+module.exports.Create = async (params) => {
+    var connection = Mysql.createConnection(global.gConfig.database);
+    var database = global.gConfig.database.name;
+
+    params = params.body // temp
+    // Object.assign(params, {codigo: uuid.v4()});
+    var query = `INSERT INTO ${database}.clientes SET ?`;
+    
+    return await new Promise((resolve) => {
+        connection.connect();
+        console.log("\nConexión establecida");
+
+        console.log(`Ejecutando consulta: ${query} ${JSON.stringify(params)}`);
+        connection.query(query, params, (error, results, fields) => {
+            connection.end();
+            console.log("Conexión cerrada");
+
+            if (error) {
+                resolve(
+                    CreateResponse(Status._500, error)
+                );
+            }
+
+            console.log(`Resultado de la consulta: ${JSON.stringify(results)}`);
+
+            resolve(
+                CreateResponse(Status._201, results)
+            );
+        });
+    });
+}
+
+
+module.exports.Update = async (req) => {
+    var connection = Mysql.createConnection(global.gConfig.database);
+    var database = global.gConfig.database.name;
+
+    var post  = req.body;
+    var query = `UPDATE ${database}.clientes SET ? WHERE id_cliente = ${post.id_cliente}`;
+    
+    return await new Promise((resolve) => {
+        connection.connect();
+        console.log("\nConexión establecida");
+
+        console.log(`Ejecutando consulta: ${query} ${JSON.stringify(post)}`);
+        connection.query(query, post, async (error, results, fields) => {
+            connection.end();
+            console.log("Conexión cerrada");
+
+            if (error) {
+                resolve(
+                    CreateResponse(Status._500, error)
+                );
+            }
+
+            if (!results.affectedRows && !results.changedRows) {
+                resolve(
+                    CreateResponse(Status._400, null)
+                );
+            }
+            console.log(`Resultado de la consulta: ${JSON.stringify(results)}`);
+
+            resolve(
+                CreateResponse(Status._202, results)
+            );
+        });
+    });
+}
+
+
+module.exports.Remove = async (req) => {
+    var connection = Mysql.createConnection(global.gConfig.database);
+    var database = global.gConfig.database.name;
+
+    var query = `DELETE FROM ${database}.clientes WHERE id_cliente = ${req.query.id_cliente}`;
+    
+    return await new Promise((resolve) => {
+        connection.connect();
+        console.log("\nConexión establecida");
+    
+        console.log(`Ejecutando consulta: ${query}`);
+        connection.query(query, (error, results, fields) => {
+            connection.end();
+            console.log("Conexión cerrada");
+
+            if (error) {
+                resolve(
+                    CreateResponse(Status._500, error)
+                );
+            }
+
+            if (!results.affectedRows && !results.changedRows) {
+                resolve(
+                    CreateResponse(Status._400, null)
+                );
+            }
+            console.log(`Resultado de la consulta: ${JSON.stringify(results)}`);
+
+            resolve(
+                CreateResponse(Status._203, results)
+            );
+        });
+    });
+}
+
--- routes/customers.js
+++ routes/customers.js
@@ -2,32 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 // Controllers
-var search = require("../controllers/customers/search");
-var create = require("../controllers/customers/create");
-var update = require("../controllers/customers/update");
-var remove = require("../controllers/customers/remove");
+var Customers = require("../controllers/customers");
 
 /* SELECT customers listing. */
 router.get('/', async (req, res, next) => {
-    let response = await search(req);
+    let response = await Customers.Search(req);
     res.status(response.status).send(response);
 });
 
 /* CREATE customers. */
 router.post('/', async (req, res, next) => {
-    let response = await create(req);
+    let response = await Customers.Create(req);
     res.status(response.status).send(response);
 });
 
 /* UPDATE customers. */
 router.put('/', async (req, res, next) => {
-    let response = await update(req);
+    let response = await Customers.Update(req);
     res.status(response.status).send(response);
 });
 
 /* DELETE customers. */
 router.delete('/', async (req, res, next) => {
-    let response = await remove(req);
+    let response = await Customers.Remove(req);
     res.status(response.status).send(response);
 });
 
